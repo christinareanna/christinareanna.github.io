bed295444e49589709b329a6732d4a96f7bd9534
Christina Knapp <christina.reanna.k@gmail.com>
2022-08-17 21:26:21 -0500

    added project images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
-// import ReservationDashboard from "../ReservationDashboard.png";
-// import NewReservation from "../NewReservation.png";
-// import NewTable from "../NewTable.png";
-// import Search from "../Search.png";
+import ReservationDashboard from "../ReservationDashboard.png";
+import NewReservation from "../NewReservation.png";
+import NewTable from "../NewTable.png";
+import Search from "../Search.png";
 import { AiOutlineGithub } from 'react-icons/ai';
+import { SiHeroku } from 'react-icons/si'
 
 
 // import Navbar from '../components/Navbar';
@@ -21,13 +22,14 @@ const Projects = ({ isOpen, toggle }) => {
             <hr style={{ border: "none" }} />
             <div className='blog-container'>
                 <h1 className='blog-title'>Thinkful Bootcamp Final Project</h1>
-                <div className='project-link'><a href='https://github.com/christinareanna/final-capstone-project-thinkful' rel='noreferrer' target='_blank' aria-label='Github'><AiOutlineGithub className="icon" size={70} /></a></div>
+                <div className='project-link'><a href='https://github.com/christinareanna/final-capstone-project-thinkful' rel='noreferrer' target='_blank' aria-label='Github'><AiOutlineGithub className="icon" size={70} /></a>
+                <a href="https://final-capstone-front-end.herokuapp.com/dashboard" rel='noreferrer' target='_blank'><SiHeroku className="icon" size={70} /></a></div>
                 <div className='project-pictures'>
                     {/* TODO- Come back to fix the images and import them again :) */}
-                    {/* <span><img className='reservation' src={ReservationDashboard} alt='reservation dashboard'></img></span>
-                    <span><img className='reservation' src={NewReservation} alt='reservation'></img></span>
-                    <span><img className='reservation' src={NewTable} alt='table'></img></span>
-                    <span><img className='reservation' src={Search} alt='search'></img></span> */}
+                    <div><img className='reservation' src={ReservationDashboard} alt='reservation' width={500} height={300}></img></div>
+                    <div><img className='reservation' src={NewReservation} alt='reservation' width={500} height={300}></img></div>
+                    <div><img className='reservation' src={NewTable} alt='table' width={500} height={300}></img></div>
+                    <div><img className='reservation' src={Search} alt='search' width={500} height={300}></img></div>
                 </div>
             </div>
             <hr style={{ border: "none" }} />
