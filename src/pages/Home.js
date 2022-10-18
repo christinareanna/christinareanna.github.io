3b053c50ca9f608bb4cd5899479a1a7c66b040f9
Christina Knapp <christina.reanna.k@gmail.com>
2022-10-17 22:54:25 -0500

    added pause project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -59,9 +59,8 @@ const Home = () => {
                 </div>
                 <img class="avatar" src={newHero} alt="" />
             </div>
-            <h1 className='home'>About me</h1>
-            <hr />
             <h2>Passionate about...</h2>
+            <hr />
             <div>
                 <div className='row'>
                     <div className='col'>
@@ -75,17 +74,18 @@ const Home = () => {
                         <ChangeIcon />
                     </div>
                 </div>
+                <h1 className='home'>About me</h1>
                 <hr />
             </div>
             <div className='global-container'>
                 <p className='global'>I started learning HTML & CSS when I was a very small, young human who had no idea the complexity that lied beneath.
-                    I've had an interest in web design for many years and decided to join a coding bootcamp called Thinkful in September 2021. I graduated in February 2022
+                    I've had an interest in design & software engineering for many years and decided to join a coding bootcamp called Thinkful in September 2021. I graduated in February 2022
                     and started at Accenture in March 2022.</p>
                 {/* <span><img className="thinkful" src={Thinkful} alt="thinkful"></img></span> */}
                 <hr />
                 <p className='global'>I feel like I've lived many lives, but that could be because I did retail for many years. I desperately wanted a change in my life and
                     did some soul searching and came to the conclusion that at the core of my being, I am an artist.
-                    I decided that web design, specifically front-end, was where my passion really was. I could finally combine everything I love into one beautiful package.
+                    I decided that software engineering, specifically front-end, was where my passion really was. I could finally combine everything I love into one beautiful package.
                     Feel free to tag along on the journey of me screaming at my computer and then successfully doing the things and stuff.</p>
             </div>
             <hr />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import ReservationDashboard from "../ReservationDashboard.png";
 import NewReservation from "../NewReservation.png";
+import PomodoroTimer from "../Pomodoro-Timer.png";
+import PomodoroTimerBreak from "../Pomodoro-Timer-Break.png";
+import LandingPage from "../landingpage.png";
 // import NewTable from "../NewTable.png";
 // import Search from "../Search.png";
 import { AiOutlineGithub } from 'react-icons/ai';
-import { SiHeroku } from 'react-icons/si'
+import { SiHeroku, SiNetlify } from 'react-icons/si'
 
 
 // import Navbar from '../components/Navbar';
@@ -21,8 +24,8 @@ const Projects = ({ isOpen, toggle }) => {
             <h1 className='projects'>Projects</h1>
             <hr style={{ border: "none" }} />
             <div className='global-container'>
-                <h1 className='global-title'>Thinkful Bootcamp Final Project</h1>
-                <h2>I'll be adding more projects soon!</h2>
+                <h1 className='global-title'>Periodic Tables</h1>
+                <p className='global-paragraph'>An application to make reservations at a restaurant.</p>
                 <div className='global'><a href='https://github.com/christinareanna/final-capstone-project-thinkful' rel='noreferrer' target='_blank' aria-label='Github'><AiOutlineGithub className="icon" size={70} /></a>
                 <a href="https://final-capstone-front-end.herokuapp.com/dashboard" rel='noreferrer' target='_blank'><SiHeroku className="icon" size={70} /></a></div>
                 <div className='project-pictures'>
@@ -31,6 +34,29 @@ const Projects = ({ isOpen, toggle }) => {
                     <div><img className='reservation' src={NewReservation} alt='reservation'></img></div>
                     {/* <div><img className='reservation' src={NewTable} alt='table' width={500} height={300}></img></div>
                     <div><img className='reservation' src={Search} alt='search' width={500} height={300}></img></div> */}
+                </div>
+            </div>
+            <hr style={{ border: "none" }} />
+            <div className='global-container'>
+                <h1 className='global-title'>Pomodoro Timer</h1>
+                <p className='global-paragraph'>A pomodoro timer that plays an audio after each focus and break session.</p>
+                <div className='global'><a href='https://github.com/christinareanna/Pomodoro-Timer' rel='noreferrer' target='_blank' aria-label='Github'><AiOutlineGithub className="icon" size={70} /></a>
+                <a href="https://christina-pomodoro-timer.netlify.app/" rel='noreferrer' target='_blank'><SiNetlify className="icon" size={70} /></a></div>
+                <div className='project-pictures'>
+                    {/* TODO- Come back to fix the images and import them again :) */}
+                    <div><img className='reservation' src={PomodoroTimer} alt='timer'></img></div>
+                    <div><img className='reservation' src={PomodoroTimerBreak} alt='timer'></img></div>
+                </div>
+            </div>
+            <hr style={{ border: "none" }} />
+            <div className='global-container'>
+                <h1 className='global-title'>Pause</h1>
+                <p className='global-paragraph'>The beginning stages of my design for a service application for service workers called Pause.</p>
+                <div className='global'><a href='https://github.com/christinareanna/serviceapplandingpage' rel='noreferrer' target='_blank' aria-label='Github'><AiOutlineGithub className="icon" size={70} /></a></div>
+                {/* <a href="https://christina-pomodoro-timer.netlify.app/" rel='noreferrer' target='_blank'><SiNetlify className="icon" size={70} /></a></div> */}
+                <div className='project-pictures'>
+                    {/* TODO- Come back to fix the images and import them again :) */}
+                    <div><img className='reservation' src={LandingPage} alt='timer'></img></div>
                 </div>
             </div>
             <hr style={{ border: "none" }} />
