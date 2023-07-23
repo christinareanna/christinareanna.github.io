8aa7b1a84fb6afa5fc62fc3efb7744a22c850d26
Christina Knapp <christina.reanna.k@gmail.com>
2023-07-23 01:37:31 -0500

    file edit

--- src/Contentful/client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "contentful";
-// const contentful = require('contentful');
-
-export const client = createClient({
-    space: 'd3o3up0gy32r',
-    accessToken: 'cBfh9186l-9NwElYAbOMc4Ufm1YnmGuKYHR0AjBCVFw',
-});
-
