5be222a5932f95acf5055880733a0b7735e66823
Christina Knapp <christina.reanna.k@gmail.com>
2024-12-18 01:26:24 -0600

    Updated ResumeLink URL

--- src/components/ResumeLink.js
+++ src/components/ResumeLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ResumeLink() {
     return (
-        <a href="https://www.linkedin.com/in/christinareannaknapp/overlay/1733792022539/single-media-viewer/?profileId=ACoAAC7u2lgB8uhNjH4B5W1K6lg6HVZwU9Mpm0E" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/christinareannaknapp/" target="_blank" rel="noreferrer">
             View my resume on LinkedIn.
         </a>
     );
