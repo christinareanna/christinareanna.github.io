54fc71aff28f7506c6d27c630ad075d874b835ef
Christina Knapp <christina.reanna.k@gmail.com>
2025-05-10 23:29:08 -0500

    updated copy portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,9 @@ import React from "react";
 import progress from "../images/1.png";
 import fitness from "../images/2.png";
 import electric from "../images/3.png";
+import headphones1 from "../images/headphones.png";
+import headphones2 from "../images/headphones2.png";
+import headphones3 from "../images/headphones3.png";
 import "./Portfolio.css";
 import Carousels from "./Carousels.js";
 
@@ -26,14 +29,24 @@ function Portfolio() {
                             Motivates the reader to embrace personal growth, boost their fitness, and consider new options.
                         </p>
                     </div>
+                    <br />
+                    <hr />
+                    <br />
+                    <img className="img10" src={headphones1} alt="copy" />
+                    <img className="img10" src={headphones2} alt="copy" />
+                    <img className="img10" src={headphones3} alt="copy" />
+                    <div className="overlay">
+                        <p>
+                            Highlights the reader's listening experience by connecting them to the emotional side of sound.
+                        </p>
+                    </div>
                 </div>
             </div>
 
             <div className="global-container">
                 <h2>The Spoonie Club Instagram Posts</h2>
-                <p>These are a few posts I created for my new Instagram group called The_Spoonie_Club.</p>
-                <br/>
-                <p>PS: A "spoonie" is someone with a chronic illness who has limited energy for daily activities.</p>
+                <p>These are a few posts I created for my new Instagram group called the_spoonie_club.
+                    A spoonie is someone with a chronic illness who has to manage their energy carefully each day.</p>
             </div>
             <Carousels />
         </div>
