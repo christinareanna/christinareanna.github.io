356ba569416b02d3e387e5007857ef84536e0da9
Christina Knapp <christina.reanna.k@gmail.com>
2025-08-16 23:58:09 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import PasswordPopup from './PasswordPopup';
-import Portfolio from './pages/Portfolio';
+// import Portfolio from './pages/Portfolio';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 // import Blogs from './pages/Blogs';
